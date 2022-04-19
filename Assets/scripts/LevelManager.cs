4dfd5febfb5fa012ff733249d9ab03098c64be96
The_Randon_Persom <therandonpersom@gmail.com>
2022-04-19 11:18:43 -0300

    level 1 design
    fiz o design do level 1

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /*public void callLevels()
     {
-        
-    }
+        SceneManager.LoadScene(PlayerPrefs.GetInt("faseAtual") + 1);
+    }*/
 }
