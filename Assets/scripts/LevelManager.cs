d2f72899840bfcae0ccbf2fc75dad53e31378197
The_Randon_Persom <therandonpersom@gmail.com>
2022-04-26 10:10:42 -0300

    level locked

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -6,11 +6,41 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
+    private bool win = false;
+    public Button[] buttons;
+
     private void Update()
     {
-        
+        winGame();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if(i + 6 > PlayerPrefs.GetInt("faseComplatada"))
+            {
+                buttons[i].interactable = false;
+            }
+           
+        }
     }
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("win") == true)
+        {
+            win = true;
+        }
+    }
+
+    void winGame()
+    {
+        if(win == true)
+        {
+            if(SceneManager.GetActiveScene().buildIndex > PlayerPrefs.GetInt("faseCompletada"))
+            {
+                PlayerPrefs.SetInt("faseCompletada", SceneManager.GetActiveScene().buildIndex);
+                PlayerPrefs.Save();
+            }
+        }
+    }
     /*public void callLevels()
     {
         SceneManager.LoadScene(PlayerPrefs.GetInt("faseAtual") + 1);
